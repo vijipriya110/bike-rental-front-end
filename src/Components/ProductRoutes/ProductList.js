780ae41a353d64bb36e3942586d480805b534e10
vijipriya110 <vijipriya110@gmail.com>
2024-03-12 11:06:46 +0530

    done

--- src/Components/ProductRoutes/ProductList.js
+++ src/Components/ProductRoutes/ProductList.js
@@ -70,7 +70,7 @@ function ProductList() {
       type="submit"
       className="flex  justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
-      buy now
+      buy rent
     </button>
   </div>
               </div>
